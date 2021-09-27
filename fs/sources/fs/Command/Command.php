4b26138381b1d6d511b8097f9923c4105f21d1ee
Loïc Brutti <loic.brutti@alzundaz.fr>
2021-09-27 20:32:07 +0200

    Display factorization

--- fs/sources/fs/Command/Command.php
+++ fs/sources/fs/Command/Command.php
@@ -17,51 +17,35 @@ abstract class Command implements IUsage
    */
   protected function usage(array $tab): int
   {
-    $description = static::getDescription();
-    if (count($description))
-    {
-      echo "Description:" . PHP_EOL;
-      foreach ($description as $line)
-      {
-        echo "  $line" . PHP_EOL;
-      }
-      echo PHP_EOL;
-    }
-
-    $usage = static::getUsage();
-    if (count($usage))
-    {
-      echo "Usage:" . PHP_EOL;
-      foreach ($usage as $line)
-      {
-        echo "  $line" . PHP_EOL;
-      }
-      echo PHP_EOL;
-    }
-
-    $examples = static::getExamples();
-    if (count($examples))
-    {
-      echo "Examples:" . PHP_EOL;
-      foreach ($examples as $line)
-      {
-        echo "  $line" . PHP_EOL;
-      }
-      echo PHP_EOL;
-    }
+    $this->displayPart('Description', static::getDescription());
+    $this->displayPart('Usage', static::getUsage());
+    $this->displayPart('Examples', static::getExamples());
 
     if (count($tab))
     {
-      $length = max(array_map("strlen", array_keys($tab)));
-      echo "Available commands:" . PHP_EOL;
+      $length = max(array_map('strlen', array_keys($tab)));
+      echo 'Available commands:' . PHP_EOL;
       foreach ($tab as $name => $class)
       {
-        echo "  $name" . str_repeat(" ", $length - strlen($name) + 1) . $class::getShortDescription() . PHP_EOL;
+        echo '  ' . $name . str_repeat(' ', $length - strlen($name) + 1) . $class::getShortDescription() . PHP_EOL;
       }
       echo PHP_EOL;
     }
 
     return 1;
+  }
+
+  private function displayPart(string $name, array $part)
+  {
+    if (count($part))
+    {
+      echo $name . ':' . PHP_EOL;
+      foreach ($part as $line)
+      {
+        echo '  ' . $line . PHP_EOL;
+      }
+      echo PHP_EOL;
+    }
   }
 
   public abstract function call(): int;
