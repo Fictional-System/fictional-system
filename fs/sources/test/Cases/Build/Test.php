5bca5729c3fd877ba6f04a37cf4b83f3175e85fb
Loïc Brutti <loic.brutti@alzundaz.fr>
2021-11-18 20:16:16 +0100

    Test multiple build.

--- fs/sources/test/Cases/Build/Test.php
+++ fs/sources/test/Cases/Build/Test.php
@@ -42,3 +42,29 @@ Tester::it('Complete build', function (ITest $tester): void {
     'argument=argument:value' . PHP_EOL .
     'build' . PHP_EOL);
 });
+
+Tester::it('Multiple build', function (ITest $tester): void {
+  $tester->shadowRun('create foo/bar/test foo/bar/foo foo/bar/bar test/bar/bar');
+  $tester->shadowRun('enable foo/bar/test foo/bar/foo test/bar/bar');
+
+  $config = new Config('test/bar/commands.json');
+  $config['default']['arguments']['argument'] = 'value';
+  $config->save();
+
+  $tester->assertRun('build', 0, '3 commands to build.');
+  $tester->assertFileExist('build.cache');
+  $tester->assertFileContent('build.cache',
+    'name=foo/bar/test' . PHP_EOL .
+    'version=latest' . PHP_EOL .
+    'context=foo/bar' . PHP_EOL .
+    'build' . PHP_EOL .
+    'name=foo/bar/foo' . PHP_EOL .
+    'version=latest' . PHP_EOL .
+    'context=foo/bar' . PHP_EOL .
+    'build' . PHP_EOL .
+    'name=test/bar/bar' . PHP_EOL .
+    'version=latest' . PHP_EOL .
+    'context=test/bar' . PHP_EOL .
+    'argument=argument:value' . PHP_EOL .
+    'build' . PHP_EOL);
+});
